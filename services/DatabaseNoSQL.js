4b529acda06f4a68e1c32ddf6ad8702d0f3e2e50
marco <mm.melchiorri@gmail.com>
2024-12-15 15:49:09 +0100

    created instance db

---
+++ services/DatabaseNoSQL.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+class NoSQLDatabase {
+    constructor() {
+        if (NoSQLDatabase.instance) {
+            return NoSQLDatabase.instance; // Return the existing instance if it already exists
+        }
+        NoSQLDatabase.instance = this; // Save the instance to the static variable
+    }
+
+    connection = async () => {
+        if (!mongoose.connection.readyState) {  // Check if the database is not already connected
+            await mongoose.connect(`${process.env.MONGO_URI}`);
+            console.log('MongoDB connected');
+        } else {
+            console.log('MongoDB already connected');
+        }
+    };
+}
+
+
+
+module.exports = new NoSQLDatabase();
