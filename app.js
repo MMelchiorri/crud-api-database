ed7279709167419568e0ce0266cfec1b09fd3db0
marco <mm.melchiorri@gmail.com>
2024-12-15 14:41:32 +0100

    created script file for installing packages

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000
+ 
+app.listen(3000, ()=>{
+    console.log(`server is listening on port ${port}`)
+})
