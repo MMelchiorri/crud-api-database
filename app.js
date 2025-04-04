c9ad10d4a8bfef9b6f4e30ecf67952cf1c77e8bb
MMelchiorri <mm.melchiorri@gmail.com>
2025-04-04 15:31:00 +0200

    feat: add CORS support and set up routing in app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
+const routes = require('./routes')
+const router = express.Router()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
+
+routes.forEach((route) => {
+  router[route.method](route.path, route.handler)
+})
 
 app.listen(3000, () => {
   console.log(`server is listening on port ${port}`)
