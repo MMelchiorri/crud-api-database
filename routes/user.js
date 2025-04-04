740375e0decad8e8de0f1ebb921939a60dfae83a
MMelchiorri <mm.melchiorri@gmail.com>
2025-04-04 15:35:59 +0200

    feat: update user routes to include path definitions for CRUD operations

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,22 @@
-import user from '../controllers/user.js'
+const user = require('../controllers/user.js')
 
 module.exports = [
   {
     method: 'GET',
+    path: '/user',
     controller: user.getAll,
   },
   {
     method: 'POST',
+    path: '/user',
     controller: user.create,
   },
   {
     method: 'PUT',
+    path: '/user/:{user}',
   },
   {
     method: 'DELETE',
+    path: '/user/:{user}',
   },
 ]
